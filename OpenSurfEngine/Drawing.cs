edc8963bea527624979d2c7579f67c5158a199c0
R3tr0BS <anonbs213@gmail.com>
2021-01-26 11:44:51 +0200

    Add project files.

---
+++ OpenSurfEngine/Drawing.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace OpenSurfEngine
+{
+    
+    static class Drawing
+    {
+        private static GameWindow win;
+        public static void Run(int width, int height) 
+        {
+            win = new GameWindow(width, height);
+            SetDelegateFunctions();
+        }
+        private static void SetDelegateFunctions() 
+        {
+            win.Load += Loaded;
+            win.RenderFrame += RenderF;
+            win.Run(1.0/60.0);
+        }
+        private static void RenderF(object sender, EventArgs e) 
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            win.SwapBuffers();
+        }
+        private static void Loaded(object sender, EventArgs e) 
+        {
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        }
+    }
+}
---
+++ OpenSurfEngine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OpenTK;
+
+namespace OpenSurfEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Drawing.Run(500, 500);
+        }
+    }
+}
